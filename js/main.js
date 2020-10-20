7541a5f1b5274655ef1a8cb57708424d7a1c1d36
loveisgala <gabriela.gk@hotmail.com>
2020-10-20 16:42:54 -0300

    feat: add var suma

--- js/main.js
+++ js/main.js
@@ -20,14 +20,13 @@
         var errorDiv = document.getElementById('error');
         var botonRegistro = document.getElementById('btnRegistro');
         var lista_productos = document.getElementById('lista-productos');
+        var suma = document.getElementById('suma-total');
 
         //Extras
         var camisas = document.getElementById('camisa_evento');
         var etiquetas = document.getElementById('etiquetas');
 
-
         calcular.addEventListener('click', calcularMontos);
-
 
         function calcularMontos(event) {
             event.preventDefault();
@@ -43,44 +42,41 @@
 
                 var totalPagar = (boletosDia * 30) + (boletos2Dias * 45) + (boletoCompleto * 50) + ((cantCamisas * 300) * .90) + (cantEtiquetas * 80);
 
-
                 var listadoProductos = [];
-
                 if (boletosDia === 1) {
                     listadoProductos.push(boletosDia + ' Pase por día');
                 } else if (boletosDia >= 1) {
                     listadoProductos.push(boletosDia + ' Pases por día');
                 }
-
                 if (boletoCompleto === 1) {
                     listadoProductos.push(boletoCompleto + ' Pase Completo');
                 } else if (boletoCompleto >= 1) {
                     listadoProductos.push(boletoCompleto + ' Pases Completos');
                 }
-
                 if (boletos2Dias === 1) {
                     listadoProductos.push(boletos2Dias + ' Pase por 2 días');
                 } else if (boletos2Dias >= 1) {
                     listadoProductos.push(boletos2Dias + ' Pases por 2 días');
                 }
-
                 if (cantCamisas === 1) {
                     listadoProductos.push(cantCamisas + ' Camisa');
                 } else if (cantCamisas >= 1) {
                     listadoProductos.push(cantCamisas + ' Camisas');
                 }
-
                 if (cantEtiquetas === 1) {
                     listadoProductos.push(cantEtiquetas + ' Etiqueta');
                 } else if (cantEtiquetas >= 1) {
                     listadoProductos.push(cantEtiquetas + ' Etiquetas');
                 }
-
+                lista_productos.style.display = "block";
                 lista_productos.innerHTML = '';
-
                 for (var i = 0; i < listadoProductos.length; i++) {
                     lista_productos.innerHTML += listadoProductos[i] + '<br/>';
                 }
+
+                suma.innerHTML = "$ " + totalPagar.toFixed(2);
+
+
             }
         }
     }); //DOM CONTENT LOADED
