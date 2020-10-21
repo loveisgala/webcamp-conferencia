eb489392ddf678b8a3d698a71793ab752ff85cf5
loveisgala <gabriela.gk@hotmail.com>
2020-10-21 15:33:09 -0300

    feat: add validarMail function

--- js/main.js
+++ js/main.js
@@ -32,6 +32,34 @@
         pase_dosdias.addEventListener('blur', mostrarDias);
         pase_completo.addEventListener('blur', mostrarDias);
 
+        nombre.addEventListener('blur', validarCampos);
+        apellido.addEventListener('blur', validarCampos);
+        email.addEventListener('blur', validarMail);
+
+        function validarCampos() {
+            if (this.value == '') {
+                errorDiv.style.display = "block";
+                errorDiv.innerHTML = "*Este campo es obligatorio."
+                this.style.border = '1px solid red';
+                errorDiv.style.color = 'red';
+            } else {
+                errorDiv.style.display = 'none';
+                this.style.border = '1px solid #cccccc';
+            }
+        }
+
+        function validarMail() {
+            if (this.value.indexOf('@') > -1) {
+                errorDiv.style.display = 'none';
+                this.style.border = '1px solid #cccccc';
+            } else {
+                errorDiv.style.display = "block";
+                errorDiv.innerHTML = "*Debe ingresar un e-mail válido."
+                this.style.border = '1px solid red';
+                errorDiv.style.color = 'red';
+            }
+        }
+
         function calcularMontos(event) {
             event.preventDefault();
             if (regalo.value === '') {
