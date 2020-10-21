565caa99e3116c04853c722adaaecaee1db7d53d
loveisgala <gabriela.gk@hotmail.com>
2020-10-21 16:11:45 -0300

    feat: change location pin

--- js/main.js
+++ js/main.js
@@ -5,14 +5,14 @@
 
     document.addEventListener('DOMContentLoaded', function() {
 
-        var map = L.map('mapa').setView([51.505, -0.09], 13);
+        var map = L.map('mapa').setView([-34.60389, -58.381568], 13);
 
         L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
             attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         }).addTo(map);
 
-        L.marker([51.5, -0.09]).addTo(map)
-            .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
+        L.marker([-34.60389, -58.381568]).addTo(map)
+            .bindPopup('Estás aquí! :)')
             .openPopup();
 
         //Campos datos usuario
