fc87b6396ce89636060730ff3888abe9485cf1a3
loveisgala <gabriela.gk@hotmail.com>
2020-10-21 21:28:31 -0300

    feat: add progama function

--- js/main.js
+++ js/main.js
@@ -146,3 +146,19 @@
 
     }); //DOM CONTENT LOADED
 })();
+
+$(function() {
+
+    //Programa de Conferencias
+    $('.programa-evento .info-curso:first').show();
+    $('.menu-programa a:first').addClass('activo');
+
+    $('.menu-programa a').on('click', function() {
+        $('.menu-programa a').removeClass('activo');
+        $(this).addClass('activo');
+        $('.ocultar').hide();
+        var enlace = $(this).attr('href');
+        $(enlace).fadeIn(1000);
+        return false;
+    });
+});
